1946476d2baa6e7d9818f204f7230b5d5ec93b88
elevenyeast <elevenyeast@gmail.com>
2015-10-31 17:41:33 +0800

    Add a page counter.

---
+++ webPagesCount.py
@@ -0,0 +1,39 @@
+import sys
+import urllib2
+import datetime
+import time
+import threading
+
+from pymongo import MongoClient
+url_count = 0
+
+# Create a mongodb connection
+client = MongoClient()
+
+# Getting a database
+db = client.url_db
+
+# Getting a collection
+collection = db.url_collection
+
+
+def init():
+ 	while True:
+ 		try:
+			timer = threading.Timer(10, writeURLCount).start()
+		except:
+			pass
+
+def writeURLCount():
+	urlCount = db.collection.count()
+
+	ts = time.time()
+	curtime = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+	writeString = "\n"+ curtime + " " + str(url_count)
+	f = open('url_count.txt', 'a')
+	f.write(writeString)
+	f.close()
+
+if __name__ == '__main__':
+    
+    init()
