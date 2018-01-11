a8da63f88f427874cff15cc39691dd8022b58ff3
Jie-Jyun Liu <jiejyun02@gmail.com>
2018-01-11 15:36:34 +0800

    Delete requests.py

--- requests.py
+++
@@ -1,2 +0,0 @@
-import requests
-res = requests.get(http://image2.pubmatic.com/AdServer/Pug?vcode=bz0yJnR5cGU9MSZqcz0xJmNvZGU9MzIxMSZ0bD0xMjk2MDA=&piggybackCookie=uid:5154155a-6093-480c-918d-2640caec8835)
